432bbf85873b715dc17a55ac30174670d57e8781
Jakub Miziołek <jakub.miziolek@contractors.roche.com>
2014-09-30 23:27:52 +0200

    enormously hard game

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ var mainState = {
 
         // Load the bird sprite
         game.load.image('bird', 'assets/bird.png'); 
+        // Load the pipe sprite
+        game.load.image('pipe', 'assets/pipe.png');  
     },
 
     create: function() {  
@@ -21,13 +23,23 @@ var mainState = {
         game.physics.arcade.enable(this.bird);
         this.bird.body.gravity.y = 1000;  
 
+        this.pipes = game.add.group(); // Create a group  
+        this.pipes.enableBody = true;  // Add physics to the group  
+        this.pipes.createMultiple(20, 'pipe'); // Create 20 pipes  
+
         // Call the 'jump' function when the spacekey is hit
         var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         spaceKey.onDown.add(this.jump, this);     
+        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);  
+        //show score
+        this.score = 0;  
+        this.labelScore = game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" }); 
     },
 
     update: function() {  
         // If the bird is out of the world (too high or too low), call the 'restartGame' function
+        //
+        game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);  
         if (this.bird.inWorld === false)
             this.restartGame();
     },
@@ -41,6 +53,33 @@ var mainState = {
     restartGame: function() {  
         // Start the 'main' state, which restarts the game
         game.state.start('main');
+    },
+    addOnePipe: function(x, y) {  
+        // Get the first dead pipe of our group
+        var pipe = this.pipes.getFirstDead();
+
+        // Set the new position of the pipe
+        pipe.reset(x, y);
+
+        // Add velocity to the pipe to make it move left
+        pipe.body.velocity.x = -200; 
+
+        // Kill the pipe when it's no longer visible 
+        pipe.checkWorldBounds = true;
+        pipe.outOfBoundsKill = true;
+    },
+    addRowOfPipes: function() {  
+        // Pick where the hole will be
+        var hole = Math.floor(Math.random() * 5) + 1;
+
+        // Add the 6 pipes 
+        for (var i = 0; i < 8; i++)
+        if (i != hole && i != hole + 1) {
+            this.addOnePipe(400, i * 60 + 10);   
+        }
+
+        this.score += 1;  
+        this.labelScore.text = this.score;  
     },
 };
 
